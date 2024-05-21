bb7ee72d736dcfa67b6f07c5130054ce16c3a5c9
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-21 14:48:20 +0200

    FIRST COMMIT

---
+++ main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            
+            
+        }
+    },
+    methods:{
+        
+
+    }
+}).mount('#app');
