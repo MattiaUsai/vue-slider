e17152e928c544367b00598385f1fd95fcb6963a
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-21 18:54:37 +0200

    ADDED COMMENT

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            // ARRAY IMMAGINI
             images:[
                 "./img/01.webp",
                 "./img/02.webp",
@@ -13,35 +14,43 @@ createApp({
                 "./img/04.webp",
                 "./img/05.webp",
             ],
-            currentIndex:2,
+            // INDICE IMMAGINE CORRENTE
+            currentIndex:0,
+            // VARIABILE MOUSE HOVER
             hover: false,
             
             
         }
     },
     methods:{
+        // FUNZIONE IMMAGINE SUCCESIVA
         nextImg(){
+            // SE IL MOUSE NON È IN HOVER SOPRA L'IMMAGINE ALLORA PASSA ALLA SUCCESSIVA ALTRIMENTI  ֻ
             if(this.hover === false){
-
+            // SE AL CLICK DEL BOTTONE L'INDICE DELL'IMMAGINEE MINORE DELLA L'UNGHEZZA DELL'ARRAY IMAGES ALLORA INCREMENTA
                 if(this.currentIndex < this.images.length-1) {
                     this.currentIndex++;
-                }else{
+                }// ALTRIMENTI IMPOSTA LA PRIMA IMMAGINE PRESENTE NELL'ARRAY
+                else{
                     this.currentIndex = 0;
                 }
             }
         },
+        // FUNZIONE IMMAGINE PRECEDENTE
         prevImg(){
+            // SE AL CLICK DEL BOTTONE L'INDICE DELL'IMMAGINEE MAGGIORE DI 0 ALLORA DECREMENTA 
             if(this.currentIndex > 0) {
                 this.currentIndex--
-            }else{
+            }// ALTRIMENTI IMPOSTA L'ULTIMA IMMAGINE PRESENTE NELL'ARRAY
+            else{
                 this.currentIndex = this.images.length-1;
             }
             
         },
+        // FUNZIONE CLICK IMMAGINE THUMB
         imgClick(i){
             this.currentIndex = i;
             
-
         },
         
           
@@ -51,12 +60,9 @@ createApp({
     },
     mounted(){
         
-        
+        // INTERVALLO IMMAGINE
         setInterval(this.nextImg, 3000);
         
-        
-          
-          
     }
     
 }).mount('#app');
