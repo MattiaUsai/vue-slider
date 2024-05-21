f6125ae8eb45128497e4ae8f2d3987272f09a385
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-21 16:11:54 +0200

    finish cursel

--- main.js
+++ main.js
@@ -6,12 +6,35 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            images:[
+                "./img/01.webp",
+                "./img/02.webp",
+                "./img/03.webp",
+                "./img/04.webp",
+                "./img/05.webp",
+            ],
+            currentIndex:2
             
             
         }
     },
     methods:{
-        
+        nextImg(){
+            if(this.currentIndex < this.images.length-1) {
+                this.currentIndex++;
+            }else{
+                this.currentIndex = 0;
+            }
+        },
+        prevImg(){
+            if(this.currentIndex > 0) {
+                this.currentIndex--
+            }else{
+                this.currentIndex = this.images.length-1;
+            }
+            
+        }
 
     }
+    
 }).mount('#app');
